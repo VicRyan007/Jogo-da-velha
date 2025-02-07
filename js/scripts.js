ed8ae40e79f8b6da82ea95c2d39eecc17dd40338
Ryan Silva <ryan.silva@showtecnologia.com>
2025-02-07 10:01:38 -0300

    Adding logic from game

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let x = document.querySelector(".x")
 let o = document.querySelector(".o")
 
-let boxes= document.querySelectorAll(".box")
+let boxes = document.querySelectorAll(".box")
 let button = document.querySelectorAll("#buttons-container button")
 let messageContainer = document.querySelector("#message")
 let messageText = document.querySelector("#message p")
@@ -14,31 +14,182 @@ let player2 = 0
 
 // Adicionando evento de click aos boxes
 
-for(let i = 0; i< boxes.length; i++){
-    boxes[i].addEventListener("click", () => {
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", function () {
 
-        let el
-
-        if (player1 == player2){
-            //x
-            el = x
-        } else {
-            //o
-            el = o
-        }
-
-        let cloneEl = el.cloneNode(true)
-
-        this.appendChild(cloneEl)
+        let el = checkEl(player1, player2)
 
 
-        //computar jogada
-        if(player1 == player2){
-            player1++
-        }else {
-            player2++
+        // verifica se tem x ou o
+        if (this.childNodes.length == 0) {
+            let cloneEl = el.cloneNode(true)
+
+            this.appendChild(cloneEl)
+
+
+            //computar jogada
+
+            if (player1 == player2) {
+                player1++
+            } else {
+                player2++
+            }
         }
 
     })
+}
+
+// verifica quem vai jogar
+function checkEl(player1, player2) {
+    if (player1 == player2) {
+        //x
+        el = x
+    } else {
+        //o
+        el = o
+    }
+
+    return el
+}
+
+// Verifica quem ganhou 
+function checkWinCondition() {
+
+    let b1 = document.getElementById("block-1")
+    let b2 = document.getElementById("block-2")
+    let b3 = document.getElementById("block-3")
+    let b4 = document.getElementById("block-4")
+    let b5 = document.getElementById("block-5")
+    let b6 = document.getElementById("block-6")
+    let b7 = document.getElementById("block-7")
+    let b8 = document.getElementById("block-8")
+    let b9 = document.getElementById("block-9")
+
+    // quadrados na horizontal
+    if (b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0) {
+
+        let b1Child = b1.childNodes[0].className
+        let b2Child = b2.childNodes[0].className
+        let b3Child = b3.childNodes[0].className
+
+        if (b1Child == "x" && b2Child == "x" && b3Child == "x") {
+
+        } else if (b1Child == "o" && b2Child == "o" && b3Child == "o") {
+
+        }
+
+    }
+
+    if (b4.childNodes.length > 0 && b5.childNodes.length > 0 && b6.childNodes.length > 0) {
+
+        let b4Child = b4.childNodes[0].className
+        let b5Child = b5.childNodes[0].className
+        let b6Child = b6.childNodes[0].className
+
+        if (b4Child == "x" && b5Child == "x" && b6Child == "x") {
+
+        } else if (b4Child == "o" && b5Child == "o" && b6Child == "o") {
+
+        }
+
+    }
+
+    if (b7.childNodes.length > 0 && b7.childNodes.length > 0 && b8.childNodes.length > 0) {
+
+        let b7Child = b7.childNodes[0].className
+        let b8Child = b8.childNodes[0].className
+        let b9Child = b9.childNodes[0].className
+
+        if (b7Child == "x" && b8Child == "x" && b9Child == "x") {
+
+        } else if (b7Child == "o" && b8Child == "o" && b9Child == "o") {
+
+        }
+    }
+    //quadrados verticais
+    if (b1.childNodes.length > 0 && b4.childNodes.length > 0 && b7.childNodes.length > 0) {
+
+        let b1Child = b1.childNodes[0].className
+        let b4Child = b4.childNodes[0].className
+        let b7Child = b7.childNodes[0].className
+
+        if (b1Child == "x" && b4Child == "x" && b7Child == "x") {
+
+        } else if (b1Child == "o" && b4Child == "o" && b7Child == "o") {
+
+        }
+
+    }
+
+    if (b2.childNodes.length > 0 && b5.childNodes.length > 0 && b8.childNodes.length > 0) {
+
+        let b2Child = b2.childNodes[0].className
+        let b5Child = b5.childNodes[0].className
+        let b8Child = b8.childNodes[0].className
+
+        if (b2Child == "x" && b5Child == "x" && b8Child == "x") {
+
+        } else if (b2Child == "o" && b5Child == "o" && b8Child == "o") {
+
+        }
+
+    }
+
+    if (b3.childNodes.length > 0 && b6.childNodes.length > 0 && b9.childNodes.length > 0) {
+
+        let b3Child = b3.childNodes[0].className
+        let b6Child = b6.childNodes[0].className
+        let b9Child = b9.childNodes[0].className
+
+        if (b3Child == "x" && b6Child == "x" && b9Child == "x") {
+
+        } else if (b3Child == "o" && b6Child == "o" && b9Child == "o") {
+
+        }
+
+    }
+
+    // diagonal
+    if (b1.childNodes.length > 0 && b5.childNodes.length > 0 && b9.childNodes.length > 0) {
+
+        let b1Child = b1.childNodes[0].className
+        let b5Child = b5.childNodes[0].className
+        let b9Child = b9.childNodes[0].className
+
+        if (b1Child == "x" && b5Child == "x" && b9Child == "x") {
+
+        } else if (b1Child == "o" && b5Child == "o" && b9Child == "o") {
+
+        }
+
+    }
+    if (b3.childNodes.length > 0 && b5.childNodes.length > 0 && b7.childNodes.length > 0) {
+
+        let b3Child = b3.childNodes[0].className
+        let b5Child = b5.childNodes[0].className
+        let b7Child = b7.childNodes[0].className
+
+        if (b3Child == "x" && b5Child == "x" && b7Child == "x") {
+
+        } else if (b3Child == "o" && b5Child == "o" && b7Child == "o") {
+
+        }
+
+    }
+
+    //deu velha
+    let counter = 0
+    for (let i = 0; i < boxes.length; i++) {
+
+        if (boxes[i].childNodes[0] != undefined) {
+            counter++
+        }
+    }
+
+    if (counter == 9) {
+        
+    }
+
+
 }
 
